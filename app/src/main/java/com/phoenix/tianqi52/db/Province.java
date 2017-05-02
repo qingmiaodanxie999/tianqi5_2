23783902a30bfcfe2c386812958467c613b3a48a
wd <397400245@qq.com>
2017-05-02 11:50:28 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/phoenix/tianqi52/db/Province.java
@@ -0,0 +1,37 @@
+package com.phoenix.tianqi52.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 王东 on 2017/5/2.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
